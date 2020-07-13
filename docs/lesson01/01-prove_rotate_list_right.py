89575a7b89e7369d47aa549d91964f952c48ec53
macbethc-byui <macbethc@byui.edu>
2020-07-13 12:36:08 -0600

    Update problem 1

---
+++ docs/lesson01/01-prove_rotate_list_right.py
@@ -0,0 +1,24 @@
+"""
+CSE212 
+(c) BYU-Idaho
+01-Prove - Problem 2
+
+It is a violation of BYU-Idaho Honor Code to post or share this code with others or 
+to post it online.  Storage into a personal and private repository (e.g. private
+GitHub repository, unshared Google Drive folder) is acceptable.
+"""
+
+def rotate_list_right(data, amount):
+    """
+    Rotate the 'data' to the right by the 
+    'amount'.   Assume that the 'amount' is 
+    valid (within the range of 1 and len(data))
+    """
+    result = None  
+    
+    return result
+
+
+print(rotate_list_right([1,2,3,4,5,6,7,8,9],1)) # [9, 1, 2, 3, 4, 5, 6, 7, 8]
+print(rotate_list_right([1,2,3,4,5,6,7,8,9],5)) # [5, 6, 7, 8, 9, 1, 2, 3, 4]
+print(rotate_list_right([1,2,3,4,5,6,7,8,9],9)) # [1, 2, 3, 4, 5, 6, 7, 8, 9]
