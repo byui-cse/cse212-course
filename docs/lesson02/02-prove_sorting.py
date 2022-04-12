00f9d89a15ed7c0f04216d02ee300529f042e807
macbeth-byui <macbethc@byui.edu>
2022-04-12 16:26:06 -0600

    Issue71

--- docs/lesson02/02-prove_sorting.py
+++ docs/lesson02/02-prove_sorting.py
@@ -10,12 +10,10 @@ GitHub repository, unshared Google Drive folder) is acceptable.
 
 def sort_list(data):
     for sort_pos in range(len(data)-1, 0, -1):
-        max_swap_pos = 0
-        for swap_pos in range(1, sort_pos+1):
-            if data[swap_pos] > data[max_swap_pos]:
-                max_swap_pos = swap_pos
-        data[max_swap_pos], data[sort_pos] = data[sort_pos], data[max_swap_pos]
-
+        for swap_pos in range(0, sort_pos):
+            if data[swap_pos] > data[swap_pos+1]:
+                data[swap_pos], data[swap_pos+1] = data[swap_pos+1], data[swap_pos]
+        
 numbers = [3,2,1,6,4,9,8]
 sort_list(numbers)
 print(numbers)  #[1, 2, 3, 4, 6, 8, 9]
